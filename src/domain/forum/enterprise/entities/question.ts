ae520865c3d04622884f642dc79fd272b462d133
Gabriel Patrício <gabrieltp087@gmail.com>
2023-11-27 12:08:24 -0300

    Create prisma repositories

--- src/domain/forum/enterprise/entities/question.ts
+++ src/domain/forum/enterprise/entities/question.ts
@@ -14,7 +14,7 @@ export interface QuestionProps {
   slug: Slug
   content: string
   createdAt: Date
-  updatedAt?: Date
+  updatedAt?: Date | null
 }
 
 export class Question extends AggregateRoot<QuestionProps> {
--- src/infra/http/controllers/create-question.controller.ts
+++ src/infra/http/controllers/create-question.controller.ts
@@ -1,10 +1,10 @@
 import { Body, Controller, Post, UseGuards } from '@nestjs/common'
-import { JwtAuthGuard } from '@/auth/jwt-auth.guard'
-import { PrismaService } from '@/prisma/prisma.service'
+import { JwtAuthGuard } from '@/infra/auth/jwt-auth.guard'
+import { PrismaService } from '@/infra/database/prisma/prisma.service'
 import { z } from 'zod'
-import { CurrentUser } from '../auth/current-user-decorator'
-import { UserPayload } from '@/auth/jwt.strategy'
-import { ZodValidationPipe } from '@/pipes/zod-validation-pipe'
+import { CurrentUser } from '../../auth/current-user-decorator'
+import { UserPayload } from '@/infra/auth/jwt.strategy'
+import { ZodValidationPipe } from '@/infra/http/pipes/zod-validation-pipe'
 
 const createQuestionBodySchema = z.object({
   title: z.string(),
